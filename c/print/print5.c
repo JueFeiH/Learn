b259bf6e50dae6a601a2e6a7ccc9aea0c6644518
JuefeiH <JuefeiH810@outlook.com>
2022-07-12 13:37:23 +0800

    ^居然不是乘方 new file:   c/print/print5.c

---
+++ c/print/print5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+//输出一个数的本身 2倍 平方
+
+int main()
+{
+    int toes = 10;
+    int toes_1 = toes*2;
+    int toes_2 = toes*toes;
+
+    printf("数字%d的两倍是%d平方是%d\n",toes,toes_1,toes_2);
+
+    return 0;
+}
